3a163bf51b5fc16542892c9b0f475ffb98f0a80b
n <n@platform500.com>
2022-12-12 11:57:14 +0300

    remove temp files + fix description in progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -13,7 +13,7 @@ def brain_progression_game():
         progression_list = list(range(randint(1, 15), 150, randint(1, 10)))
         hidden_number = progression_list[d] 
         progression_list[d] = '..'
-        print(f'Question: {progression_list[0:10]}')
+        print('Question: ' + " ".join(map(str, progression_list[0:10])))
         cli.check_answer(hidden_number, prompt.string('Your answer: ', empty=True))
         i += 1
 
